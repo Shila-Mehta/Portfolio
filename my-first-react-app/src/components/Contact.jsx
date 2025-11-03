f94385e90892bd261dcd2172c9413a3c293b7903
Nimra Jabbar <nimraabduljabbar14@gmail.com>
2025-11-03 17:16:52 +0500

    updated portfolio

--- my-first-react-app/src/components/Contact.jsx
+++ my-first-react-app/src/components/Contact.jsx
@@ -7,7 +7,7 @@ export default  function Contact(){
             <h2>Contact Me</h2>
             <p> I love to connect - whether its for work ,collaboration or just to say hi</p>
             <ul  className='contact-links'>
-                <li><a href='nimraabduljabbar14@gmail.com' target='_blank' rel="noopener noreferrer">Email</a></li>
+                <li><a href='#' target='_blank' rel="noopener noreferrer">Email:nimraabduljabbar14@gmail.com</a></li>
                 <li><a href='https://github.com/Shila-Mehta' target='_blank' rel="noopener noreferrer">GitHub</a></li>
                 <li><a href='https://www.linkedin.com/in/n-jabbar-1113aa374/' target='_blank' rel="noopener noreferrer">LinkedIn</a></li>
             </ul>
