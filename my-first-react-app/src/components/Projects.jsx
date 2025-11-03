20b2eb30df98280b7d7e57987eab9ba874f3c954
Nimra Jabbar <nimraabduljabbar14@gmail.com>
2025-11-04 02:02:16 +0500

    updated portfolio

--- my-first-react-app/src/components/Projects.jsx
+++ my-first-react-app/src/components/Projects.jsx
@@ -1,136 +1,157 @@
 import '../styles/projects.css';
 
 
-const projects=[
+const projects = [
+
 
   {
-        title:'Shopping Cart',
-        description: "A dynamic e-commerce shopping cart application that allows users to browse products, add items to the cart, update quantities, and calculate totals in real-time.",
-        technologies:['React','Tailwind',"fakeClothing Api"],
-        image: "/Images/Shopping Cart.png"  ,
-        liveLink:'https://shopping-cart-nimra.netlify.app/',
-        githubLink:'https://github.com/Shila-Mehta/Shopping-Cart.git',  
-    },
+    title: 'E-Commerce Website',
+    description: "A full-stack MERN e-commerce platform featuring secure user authentication, product management, cart functionality, Stripe payments, and an admin dashboard. Built with modern UI and RESTful API architecture.",
+    technologies: ['MongoDB', 'Express', 'React', 'Node.js', 'Stripe', 'Tailwind', "JWT", "NodeMailer", "Multer", "bcrypt", "pdfkit"],
+    image: '/Images/ecommerce-website-homePage.png',
+    liveLink: 'https://github.com/Shila-Mehta/ecommerce-website.git',
+    githubLink: 'https://github.com/Shila-Mehta/ecommerce-website.git',
+  },
 
-     {
-        title:'Memory Card Game',
-        description: "An interactive memory matching game where users flip cards to find matching pairs. The game tracks score, best score, and includes dynamic content fetched from external APIs (e.g., Pokémon or Giphy).",
-        technologies:['HTML','webpack','CSS','React','Pokemon Api'],
-        image: "/Images/memory-card-game.png"  ,
-        liveLink:'https://shopping-cart-nimra.netlify.app/',
-        githubLink:'https://github.com/Shila-Mehta/Shopping-Cart.git',  
-    },
 
   {
-        title:'CV Builder',
-        description: "A dynamic CV builder application that allows users to create, preview, and print professional resumes. Features include live editing, customizable sections, and real-time preview of the CV layout.",
-        technologies:['HTML','webpack','CSS','React'],
-        image: "/Images/cv-builder-nimra.netlify.app_ (1).png"  ,
-        liveLink:'https://cv-builder-nimra.netlify.app/',
-        githubLink:'https://github.com/Shila-Mehta/CV-Builder.git',  
-    },
+    title: 'Inventory Management System',
+    description: "A full-stack inventory management application built with Node.js, Express, and EJS. Users can efficiently track products, manage stock levels, and update inventory through a responsive, user-friendly interface. The app uses PostgreSQL for data storage.",
+    technologies: ['Node.js', 'Express', 'EJS', 'CSS', 'JavaScript', 'PostgreSQL'],
+    image: "/Images/Inventry.png",
+    liveLink: 'https://github.com/Shila-Mehta/Inventry-Application.git', // replace with deployed link if available
+    githubLink:'https://github.com/Shila-Mehta/Inventry-Application.git',
+  },
 
-    {
-        title:'BattleShip  Game',
-        description: "A two-player turn-based Battleship game built with JavaScript and DOM manipulation. Includes drag-and-drop ship placement and hit detection.",
-        technologies:['HTML','webpack','CSS','Jest','Javascript'],
-        image: "/Images/BattleShip.png"  ,
-        liveLink:'https://shila-mehta.github.io/Battleship/',
-        githubLink:'https://github.com/Shila-Mehta/Battleship.git',  
-    },
 
-    {
-        title:'Weather App',
-        description:'A  responsive app  built    that fetches real-time weather data  from visual-crossing Includes search,loading state and animated icons',
-        technologies:['HTML','webpack','CSS','API','Javascript'],
-        image: "/Images/Weather.png"  ,
-        liveLink:'https://shila-mehta.github.io/WeatherApp/',
-        githubLink:'https://github.com/Shila-Mehta/WeatherApp.git',
-    }
-    ,
+  {
+    title: 'Shopping Cart',
+    description: "A dynamic e-commerce shopping cart application that allows users to browse products, add items to the cart, update quantities, and calculate totals in real-time.",
+    technologies: ['React', 'Tailwind', "fakeClothing Api"],
+    image: "/Images/Shopping Cart.png",
+    liveLink: 'https://shopping-cart-nimra.netlify.app/',
+    githubLink: 'https://github.com/Shila-Mehta/Shopping-Cart.git',
+  },
 
-    {
-        title:'Todo List',
-        description:'A  responsive app  built    that fetches real-time weather data  from visual-crossing Includes search,loading state and animated icons',
-        technologies:['HTML','webpack','CSS','Javascript'],
-        image: '/Images/TodoList.png'  , 
-        liveLink:'https://shila-mehta.github.io/Todo-List/',
-        githubLink:'https://github.com/Shila-Mehta/Todo-List.git',
-    }
-    ,
-    {
-        title:'Restaurant Page',
-        description:'A  responsive app  built    that fetches real-time weather data  from visual-crossing Includes search,loading state and animated icons',
-        technologies:['HTML','webpack','CSS','Javascript'],
-        image: '/Images/Restaurant.png' ,
-        liveLink:'https://shila-mehta.github.io/Restaurant/',
-        githubLink:'https://github.com/Shila-Mehta/Restaurant.git',
-    },
+  {
+    title: 'Memory Card Game',
+    description: "An interactive memory matching game where users flip cards to find matching pairs. The game tracks score, best score, and includes dynamic content fetched from external APIs (e.g., Pokémon or Giphy).",
+    technologies: ['HTML', 'webpack', 'CSS', 'React', 'Pokemon Api'],
+    image: "/Images/memory-card-game.png",
+    liveLink: 'https://shopping-cart-nimra.netlify.app/',
+    githubLink: 'https://github.com/Shila-Mehta/Shopping-Cart.git',
+  },
 
-    {
-        title:'Tic  Tac Toe',
-        description:'A  responsive app  built    that fetches real-time weather data  from visual-crossing Includes search,loading state and animated icons',
-        technologies:['HTML','webpack','CSS','Javascript'],
-        image: '/Images/Tic Tac Toe.png'  ,
-        liveLink:'https://shila-mehta.github.io/TicTacToe/',
-        githubLink:'https://github.com/Shila-Mehta/TicTacToe.git',
-    },
+  {
+    title: 'CV Builder',
+    description: "A dynamic CV builder application that allows users to create, preview, and print professional resumes. Features include live editing, customizable sections, and real-time preview of the CV layout.",
+    technologies: ['HTML', 'webpack', 'CSS', 'React'],
+    image: "/Images/cv-builder-nimra.netlify.app_ (1).png",
+    liveLink: 'https://cv-builder-nimra.netlify.app/',
+    githubLink: 'https://github.com/Shila-Mehta/CV-Builder.git',
+  },
 
-    {
-        title:'Library',
-        description:'A  responsive app  built    that fetches real-time weather data  from visual-crossing Includes search,loading state and animated icons',
-        technologies:['HTML','webpack','CSS','Javascript'],
-        image: '/Images/Library.png'  ,
-        liveLink:'https://shila-mehta.github.io/Libraray/',
-        githubLink:'https://github.com/Shila-Mehta/Libraray.git',
-    },
+  {
+    title: 'BattleShip  Game',
+    description: "A two-player turn-based Battleship game built with JavaScript and DOM manipulation. Includes drag-and-drop ship placement and hit detection.",
+    technologies: ['HTML', 'webpack', 'CSS', 'Jest', 'Javascript'],
+    image: "/Images/BattleShip.png",
+    liveLink: 'https://shila-mehta.github.io/Battleship/',
+    githubLink: 'https://github.com/Shila-Mehta/Battleship.git',
+  },
 
-    {
-        title:'Admin Dashboard',
-        description:'A  responsive app  built    that fetches real-time weather data  from visual-crossing Includes search,loading state and animated icons',
-        technologies:['HTML','webpack','CSS'],
-        image: '/Images/Admin Dashboard.png'  ,
-        liveLink:'https://shila-mehta.github.io/my_dashboard/',
-        githubLink:'https://github.com/Shila-Mehta/my_dashboard.git',
-    },
+  {
+    title: 'Weather App',
+    description: 'A  responsive app  built    that fetches real-time weather data  from visual-crossing Includes search,loading state and animated icons',
+    technologies: ['HTML', 'webpack', 'CSS', 'API', 'Javascript'],
+    image: "/Images/Weather.png",
+    liveLink: 'https://shila-mehta.github.io/WeatherApp/',
+    githubLink: 'https://github.com/Shila-Mehta/WeatherApp.git',
+  }
+  ,
 
-    {
-        title:'Sign Up Form',
-        description:'A  responsive app  built    that fetches real-time weather data  from visual-crossing Includes search,loading state and animated icons',
-        technologies:['HTML','CSS'],
-        image:  '/Images/Sign-up.png' ,
-        liveLink:'https://shila-mehta.github.io/Sign-up-form/',
-        githubLink:'https://github.com/Shila-Mehta/Sign-up-form.git',
-    }
-    ,
-   
-    {
-        title:'Calculator',
-        description:'A  responsive app  built    that fetches real-time weather data  from visual-crossing Includes search,loading state and animated icons',
-        technologies:['HTML','CSS'],
-        image: '/Images/Calculator.png'  ,
-        liveLink:'https://shila-mehta.github.io/Calculator/',
-        githubLink:'https://github.com/Shila-Mehta/Calculator.git',
-    },
+  {
+    title: 'Todo List',
+    description: 'A  responsive app  built    that fetches real-time weather data  from visual-crossing Includes search,loading state and animated icons',
+    technologies: ['HTML', 'webpack', 'CSS', 'Javascript'],
+    image: '/Images/TodoList.png',
+    liveLink: 'https://shila-mehta.github.io/Todo-List/',
+    githubLink: 'https://github.com/Shila-Mehta/Todo-List.git',
+  }
+  ,
+  {
+    title: 'Restaurant Page',
+    description: 'A  responsive app  built    that fetches real-time weather data  from visual-crossing Includes search,loading state and animated icons',
+    technologies: ['HTML', 'webpack', 'CSS', 'Javascript'],
+    image: '/Images/Restaurant.png',
+    liveLink: 'https://shila-mehta.github.io/Restaurant/',
+    githubLink: 'https://github.com/Shila-Mehta/Restaurant.git',
+  },
 
-    {
-        title:'Drop Down Menu',
-        description:'A  responsive app  built    that fetches real-time weather data  from visual-crossing Includes search,loading state and animated icons',
-        technologies:['HTML','CSS','Javascript'],
-        image: '/Images/DropDownMenu.png'  ,
-        liveLink:'https://shila-mehta.github.io/DropDown-Menu/',
-        githubLink:'https://github.com/Shila-Mehta/DropDown-Menu.git',
-    },
+  {
+    title: 'Tic  Tac Toe',
+    description: 'A  responsive app  built    that fetches real-time weather data  from visual-crossing Includes search,loading state and animated icons',
+    technologies: ['HTML', 'webpack', 'CSS', 'Javascript'],
+    image: '/Images/Tic Tac Toe.png',
+    liveLink: 'https://shila-mehta.github.io/TicTacToe/',
+    githubLink: 'https://github.com/Shila-Mehta/TicTacToe.git',
+  },
 
-    {
-        title:'Image Carousel',
-        description:'A  responsive app  built    that fetches real-time weather data  from visual-crossing Includes search,loading state and animated icons',
-        technologies:['HTML','webpack','CSS','Javascript'],
-        image: '/Images/Image-carousel.png'  ,
-        liveLink:'https://shila-mehta.github.io/Image-carousel/',
-        githubLink:'https://github.com/Shila-Mehta/Image-carousel.git',
-    }
-  
+  {
+    title: 'Library',
+    description: 'A  responsive app  built    that fetches real-time weather data  from visual-crossing Includes search,loading state and animated icons',
+    technologies: ['HTML', 'webpack', 'CSS', 'Javascript'],
+    image: '/Images/Library.png',
+    liveLink: 'https://shila-mehta.github.io/Libraray/',
+    githubLink: 'https://github.com/Shila-Mehta/Libraray.git',
+  },
+
+  {
+    title: 'Admin Dashboard',
+    description: 'A  responsive app  built    that fetches real-time weather data  from visual-crossing Includes search,loading state and animated icons',
+    technologies: ['HTML', 'webpack', 'CSS'],
+    image: '/Images/Admin Dashboard.png',
+    liveLink: 'https://shila-mehta.github.io/my_dashboard/',
+    githubLink: 'https://github.com/Shila-Mehta/my_dashboard.git',
+  },
+
+  {
+    title: 'Sign Up Form',
+    description: 'A  responsive app  built    that fetches real-time weather data  from visual-crossing Includes search,loading state and animated icons',
+    technologies: ['HTML', 'CSS'],
+    image: '/Images/Sign-up.png',
+    liveLink: 'https://shila-mehta.github.io/Sign-up-form/',
+    githubLink: 'https://github.com/Shila-Mehta/Sign-up-form.git',
+  }
+  ,
+
+  {
+    title: 'Calculator',
+    description: 'A  responsive app  built    that fetches real-time weather data  from visual-crossing Includes search,loading state and animated icons',
+    technologies: ['HTML', 'CSS'],
+    image: '/Images/Calculator.png',
+    liveLink: 'https://shila-mehta.github.io/Calculator/',
+    githubLink: 'https://github.com/Shila-Mehta/Calculator.git',
+  },
+
+  {
+    title: 'Drop Down Menu',
+    description: 'A  responsive app  built    that fetches real-time weather data  from visual-crossing Includes search,loading state and animated icons',
+    technologies: ['HTML', 'CSS', 'Javascript'],
+    image: '/Images/DropDownMenu.png',
+    liveLink: 'https://shila-mehta.github.io/DropDown-Menu/',
+    githubLink: 'https://github.com/Shila-Mehta/DropDown-Menu.git',
+  },
+
+  {
+    title: 'Image Carousel',
+    description: 'A  responsive app  built    that fetches real-time weather data  from visual-crossing Includes search,loading state and animated icons',
+    technologies: ['HTML', 'webpack', 'CSS', 'Javascript'],
+    image: '/Images/Image-carousel.png',
+    liveLink: 'https://shila-mehta.github.io/Image-carousel/',
+    githubLink: 'https://github.com/Shila-Mehta/Image-carousel.git',
+  }
+
 ]
 
 
