e57c1ad69d33dccbd5096ffb520cef9f413ccc19
Nimra Jabbar <nimraabduljabbar14@gmail.com>
2025-11-03 17:13:50 +0500

    updated portfolio

--- my-first-react-app/src/components/Contact.jsx
+++ my-first-react-app/src/components/Contact.jsx
@@ -7,9 +7,9 @@ export default  function Contact(){
             <h2>Contact Me</h2>
             <p> I love to connect - whether its for work ,collaboration or just to say hi</p>
             <ul  className='contact-links'>
-                <li><a href='https://mail.google.com/mail/u/0/?pli=1#inbox' target='_blank' rel="noopener noreferrer">Email</a></li>
-                <li><a href='https://github.com/' target='_blank' rel="noopener noreferrer">GitHub</a></li>
-                <li><a href='https://www.linkedin.com/feed/' target='_blank' rel="noopener noreferrer">LinkedIn</a></li>
+                <li><a href='nimraabduljabbar14@gmail.com' target='_blank' rel="noopener noreferrer">Email</a></li>
+                <li><a href='https://github.com/Shila-Mehta' target='_blank' rel="noopener noreferrer">GitHub</a></li>
+                <li><a href='https://www.linkedin.com/in/n-jabbar-1113aa374/' target='_blank' rel="noopener noreferrer">LinkedIn</a></li>
             </ul>
         </section>
 
--- my-first-react-app/src/components/Footer.jsx
+++ my-first-react-app/src/components/Footer.jsx
@@ -5,8 +5,8 @@ export default function Footer(){
         <footer  className='footer'>
             <p>© {new Date().getFullYear()} Nimra Abdul Jabbar,All rights reserved .</p>
             <div className='footer-links'>
-                <a href='https://github.com/' target='_blank'  rel="noopener noreferrer">GitHub</a>
-                <a href='https://www.linkedin.com/feed/' target='_blank'  rel="noopener noreferrer">LinkedIn</a>
+                <a href='https://github.com/Shila-Mehta' target='_blank'  rel="noopener noreferrer">GitHub</a>
+                <a href='https://www.linkedin.com/in/n-jabbar-1113aa374/' target='_blank'  rel="noopener noreferrer">LinkedIn</a>
             </div>
         </footer>
     )
